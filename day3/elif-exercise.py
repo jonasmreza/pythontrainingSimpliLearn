387c7935e80eaeeeb194e39ebb1ef9820b72aea7
Montoya <jmontoyar@MX.lala.enterprise>
2022-03-18 23:26:52 -0700

    add def factorial

---
+++ day3/elif-exercise.py
@@ -0,0 +1,9 @@
+n=int(input("enter any number: "))
+if(n==1):
+    print("ONE")
+elif(n==2):
+    print("two")
+elif(n==3):
+    print("three")
+else:
+    print("Invalid number")
---
+++ day3/venn-if.py
@@ -0,0 +1,21 @@
+Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=5;
+print(a)
+5
+a==5;
+True
+a="Learning is an art"
+b="art"
+b in a
+True
+data={4,5,6,7,8}
+9 in data
+False
+4 in data
+True
+x=6
+if x>5:
+    print("hi these are conditional expresion")
+
+hi these are conditional expresion
