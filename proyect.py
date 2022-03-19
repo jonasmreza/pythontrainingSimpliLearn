eebf8c235789678493dfcc2f394aaaa58d904990
Montoya <jmontoyar@MX.lala.enterprise>
2022-03-18 17:28:39 -0700

    Creacion de repositorio

---
+++ proyect.py
@@ -0,0 +1,12 @@
+print("Hi")
+
+for i in range (1,6):
+    print(i)
+    
+
+name="meg"
+work="teaching"
+exp="16"
+
+print("i am"+name+"and "+work+" for last "+exp+" years")
+
---
+++ proyecto2.py
@@ -0,0 +1,4 @@
+name=input("enter your name:")
+age=int(input("Enter your age:"))
+
+print("Your Name is: ",name, "Your Age is: ",age)
