1138cfb02752fb246b0b5d84c0cc07f34b97bc6c
Montoya <jmontoyar@MX.lala.enterprise>
2022-03-25 23:03:01 -0700

    half first

---
+++ day5/diamonpatern.py
@@ -0,0 +1,18 @@
+n=int(input("enter row: ")) #5
+
+#print spaces
+
+for r in range(1,2*n):
+    #for spaces
+    for s in range(n,r,-1):
+        print(" ",end="")
+    for st in range(1,2*r):
+        print("*",end="")
+    print()
+#second half
+for r in range(n,0,-1):
+    for s in range(n,r,1):
+        print("*",end="")
+    for st in range(1,2*r):
+        print("*",end="")
+    print("")
